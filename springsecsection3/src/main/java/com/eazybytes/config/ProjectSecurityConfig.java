a6b0aa2fcce5d97d14f4653686c03f7343468573
Ramesh <krishnaraju.chiluvuri@gmail.com>
2024-07-15 11:44:40 +0530

    initial commit

--- springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -9,10 +9,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -25,16 +29,18 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> {
+        http.csrf((req)->req.disable())
+                .authorizeHttpRequests((requests) -> {
             requests.requestMatchers("/myaccount","/myBalance","/myLoans","/myCards").authenticated();
-            requests.requestMatchers("/notices","/contact").permitAll();
+            requests.requestMatchers("/notices","/contact","/register").permitAll();
         });
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.csrf(req->req.disable());
         return (SecurityFilterChain)http.build();
     }
 
-    @Bean
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsService(){
 
         //Approach1 where we use
@@ -51,7 +57,16 @@ public class ProjectSecurityConfig {
 
         return new InMemoryUserDetailsManager(admin,user);
 
-    }
+    }*/
+
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+         return new JdbcUserDetailsManager(dataSource);
+    }*/
+
+
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
